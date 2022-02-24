d885c12c591d1d19787c14f7e35e7927ab075faa
Cedrick Flocon <7161169+CedrickFlocon@users.noreply.github.com>
2022-02-24 17:15:14 +0100

    Data module and graphql setup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,7 @@ include(
 )
 
 include(
+    ":user:data",
     ":user:domain",
     ":user:presentation",
 )
---
+++ user/data/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("kotlin")
+    alias(libs.plugins.apollo.graphql.plugin)
+}
+
+apollo {
+    packageName.set("com.cedrickflocon.android.showcase.user.data")
+    mapScalar("URI", "java.net.URI")
+}
+
+tasks.test { useJUnitPlatform() }
+
+dependencies {
+    implementation(project(":user:domain"))
+
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.apollo.graphql.runtime)
+    implementation(libs.arrow.core)
+    implementation(libs.kotlin.coroutine)
+
+    testImplementation(libs.kotest)
+    testImplementation(libs.mockk)
+    testImplementation(libs.truth)
+    testImplementation(libs.truth.extensions)
+}
