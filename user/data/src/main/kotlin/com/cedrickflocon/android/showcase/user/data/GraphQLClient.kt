16dc7a614a00a5ecdcd36b714705e26ff241bfe7
Cedrick Flocon <7161169+CedrickFlocon@users.noreply.github.com>
2022-03-07 19:28:23 +0100

    Add access token

--- user/data/src/main/kotlin/com/cedrickflocon/android/showcase/user/data/GraphQLClient.kt
+++ user/data/src/main/kotlin/com/cedrickflocon/android/showcase/user/data/GraphQLClient.kt
@@ -8,10 +8,30 @@ import okhttp3.OkHttpClient
 import okhttp3.Response
 import javax.inject.Inject
 
-class GraphQLClient @Inject constructor() {
+class GraphQLClient @Inject constructor(
+    private val bearer: String
+) {
 
     val client = ApolloClient.Builder()
         .serverUrl("https://api.github.com/graphql")
+        .addHttpInterceptor(LoggingInterceptor())
+        .okHttpClient(
+            OkHttpClient.Builder()
+                .addInterceptor(AuthorizationInterceptor(bearer))
+                .build()
+        )
         .build()
+
+    private class AuthorizationInterceptor(
+        private val bearer: String
+    ) : Interceptor {
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val request = chain.request().newBuilder()
+                .addHeader("Authorization", "bearer $bearer")
+                .build()
+
+            return chain.proceed(request)
+        }
+    }
 
 }
